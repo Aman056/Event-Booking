daa0237992b4caabdb2e1dc80f5b60e977282ff0
Aman056 <vijaychau056@gmail.com>
2024-03-23 18:45:19 +0530

    the

---
+++ src/Components/Event.js
@@ -0,0 +1,146 @@
+import React from 'react'
+
+export default function Event() {
+    return (
+        <div className='bg-slate-0 py-2 mt-6'>
+            <span className='flex justify-start font-bold'>Event Option</span>
+            {/* this is */}
+            <div className='bg-slate-100 flex items-center justify-between px-1 py-3 rounded-lg my-1'>
+                <div className='flex items-center'>
+                    <div className='icon'>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-4 h-4 mx-1  text-slate-400"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 0 1 0 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 0 1 0-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375Z"
+                            />
+                        </svg>
+                    </div>
+                    <div className='icon text-slate-400'><span>Tickets</span></div>
+                </div>
+                <div className='flex items-center'>
+                    <div className='icon  text-slate-400'><span>Free</span></div>
+                    <div className='icon'>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-4 h-4 mx-1  text-slate-400"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="m18.375 12.739-7.693 7.693a4.5 4.5 0 0 1-6.364-6.364l10.94-10.94A3 3 0 1 1 19.5 7.372L8.552 18.32m.009-.01-.01.01m5.699-9.941-7.81 7.81a1.5 1.5 0 0 0 2.112 2.13"
+                            />
+                        </svg>
+                    </div>
+                </div>
+            </div>
+            {/* this is */}
+
+            <div className='bg-slate-100 flex items-center justify-between px-1 py-3 rounded-lg my-1'>
+                <div className='flex items-center'>
+                    <div className='icon'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 mx-1  text-slate-400">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 48.678 48.678 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 0 0 3.7 3.7 48.656 48.656 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3-3 3" />
+                        </svg>
+                    </div>
+                    <div className='icon text-slate-400'><span>Request</span></div>
+                </div>
+                <div className='flex items-center'>
+                    <div className='icon  text-slate-400'><span>Free</span></div>
+                    <div className='icon'>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-4 h-4 mx-1  text-slate-400"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="m18.375 12.739-7.693 7.693a4.5 4.5 0 0 1-6.364-6.364l10.94-10.94A3 3 0 1 1 19.5 7.372L8.552 18.32m.009-.01-.01.01m5.699-9.941-7.81 7.81a1.5 1.5 0 0 0 2.112 2.13"
+                            />
+                        </svg>
+                    </div>
+                </div>
+            </div>
+
+            {/* this is */}
+
+            <div className='bg-slate-100 flex items-center justify-between px-1 py-3 rounded-lg my-1'>
+                <div className='flex items-center'>
+                    <div className='icon'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 mx-1  text-slate-400">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m21 7.5-2.25-1.313M21 7.5v2.25m0-2.25-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3 2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75 2.25-1.313M12 21.75V19.5m0 2.25-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25" />
+                        </svg>
+                    </div>
+                    <div className='icon text-slate-400'><span>Tickets</span></div>
+                </div>
+                <div className='flex items-center'>
+                    <div className='icon  text-slate-400'><span>Free</span></div>
+                    <div className='icon'>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-4 h-4 mx-1  text-slate-400"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="m18.375 12.739-7.693 7.693a4.5 4.5 0 0 1-6.364-6.364l10.94-10.94A3 3 0 1 1 19.5 7.372L8.552 18.32m.009-.01-.01.01m5.699-9.941-7.81 7.81a1.5 1.5 0 0 0 2.112 2.13"
+                            />
+                        </svg>
+                    </div>
+                </div>
+            </div>
+            {/* this is */}
+            <div className='bg-slate-100 flex items-center justify-between px-1 py-3 rounded-lg my-1'>
+                <div className='flex items-center'>
+                    <div className='icon'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 mx-1  text-slate-400">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                        </svg>
+
+                    </div>
+                    <div className='icon text-slate-400'><span>Tickets</span></div>
+                </div>
+                <div className='flex items-center'>
+                    <div className='icon  text-slate-400'><span>Free</span></div>
+                    <div className='icon'>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-4 h-4 mx-1  text-slate-400"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="m18.375 12.739-7.693 7.693a4.5 4.5 0 0 1-6.364-6.364l10.94-10.94A3 3 0 1 1 19.5 7.372L8.552 18.32m.009-.01-.01.01m5.699-9.941-7.81 7.81a1.5 1.5 0 0 0 2.112 2.13"
+                            />
+                        </svg>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
--- src/Components/ScheduleInvite.js
+++ src/Components/ScheduleInvite.js
@@ -44,7 +44,7 @@ export default function ScheduleInvite(props) {
                                         <button className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-4 rounded mt-4">
                                             Invited
                                         </button>
-                                        <div className="flex -space-x-1 overflow-hidden mt-4 mx-2">
+                                        <div className="flex -space-x-1 overflow-hidden mt-5 mx-2">
                                             <img
                                                 className="inline-block h-5 w-5 rounded-full ring-2 ring-white"
                                                 src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
